bfb52789719651b89814450379081c4d90c991a0
jackinabox <adrian.defer@gmail.com>
2018-10-28 15:06:40 +0100

    works

--- catkin_ws/src/assignment1_publisher_subscriber/src/publisher_subscriber.py
+++ catkin_ws/src/assignment1_publisher_subscriber/src/publisher_subscriber.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 # assignment 2
 
 import math
@@ -6,17 +8,17 @@ import rospy
 from std_msgs.msg import Float32, String
 
 def callback(yaw_msg):
-	deg = round(math.degrees(yaw_msg),1)
-	publisher.publish(output % (yaw_msg, deg))
+	deg = round(math.degrees(float(yaw_msg.data)),2)
+	publisher.publish(String(output % (yaw_msg.data, deg)))
 
 # Initialize node
 rospy.init_node("echo_yaw_node")
 
 # output format
-output = "I heard: %f (~%.1f°)"
+output = "I heard: %f (%.2f deg)"
 
 # Initialize publisher and subscriber
-publisher = rospy.Publisher("/assignment1_publisher_subscriber", String)
+publisher = rospy.Publisher("/assignment1_publisher_subscriber", String, queue_size=10)
 rospy.Subscriber("/yaw", Float32, callback)
 
 # spin() keeps python from exiting until this node is stopped
